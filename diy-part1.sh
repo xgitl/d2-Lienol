fbbece415c5988debdbdd3ffaee19eaccf0aa417
xgitl <69244012+xgitl@users.noreply.github.com>
2023-04-20 10:36:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,7 @@ sed -i '/^src-git other/d' feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
 echo 'src-git neteasemusic https://github.com/cnsilvan/luci-app-unblockneteasemusic' >>feeds.conf.default
+svn co https://github.com/Lienol/openwrt-package/branches/other/lean/luci-app-turboacc package/luci-app-turboacc
 svn co https://github.com/Lienol/openwrt-package/branches/other/lean/adbyby package/adbyby
 svn co https://github.com/Lienol/openwrt-package/branches/other/lean/luci-app-adbyby-plus package/luci-app-adbyby-plus
 # git clone https://github.com/ilxp/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
